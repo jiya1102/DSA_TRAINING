0df3c4938bbe27619a4563f732cac52b90f75f35
jiya1102 <sonijiya752@gmail.com>
2026-03-28 16:57:57 +0530

    updated commit

---
+++ LINKED_LIST/Insert_at_end.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+typedef struct node{
+   int data;
+   struct node *next;
+}Node;
+
+Node* getnode(int);
+Node* build123();
+void display(Node *);
+void InsertAtEnd(node*,int);
+
+int main(){
+   Node *HP;
+   HP=build123();
+   InsertAtEnd(HP,4);
+   display(HP); 
+   
+}
+
+Node* getnode(int x){//creat a node and return address of node
+   Node *p;
+   p= (Node*) malloc(sizeof(Node));
+   p->data=x;
+   p->next=NULL;
+   return p;
+}
+
+Node* build123(){ //creat a list 1,2,3 and return address of first node
+    Node* temp1,*temp2,*temp3;
+    temp1=getnode(1);
+    temp2=getnode(2);
+    temp3=getnode(3);
+
+    temp1->next=temp2;
+    temp2->next=temp3;
+    return temp1;
+}
+
+void display(Node *s){//count node of list 
+    int count =0; 
+    while(s!=NULL){
+        count ++;
+     printf("%d \n",s->data );
+     s=s->next;
+    } cout<<"no. of node is:"<<count<<endl;
+}
+
+ void InsertAtEnd(node*s,int x){
+    node *A;
+    A =getnode(x);
+    
+   while(s->next!=NULL){
+      s=s->next;
+   }
+   s->next=A;
+  
+}
+
