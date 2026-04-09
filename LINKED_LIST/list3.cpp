18b44bc7da5cd5969208f4b66eac81138714f7fe
jiya1102 <sonijiya752@gmail.com>
2026-04-09 09:52:58 +0530

    updated commit

---
+++ LINKED_LIST/list3.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+typedef struct node{
+   int data;
+   struct node *next;
+}Node;
+
+Node* getnode(int);
+Node* build123();
+void display(Node *);
+
+int main(){
+   Node *HP;
+   HP=build123();
+   display(HP); 
+}
+
+Node* getnode(int x){//creat a node and return address of node
+   Node *p;
+   p= (Node*) malloc(sizeof(Node));
+   p->data=x;
+   p->next=NULL;
+   return p;
+}
+
+Node* build123(){ //creat a list 1,2,3 and return address of first node
+    Node* temp1,*temp2,*temp3;
+    temp1=getnode(1);
+    temp2=getnode(2);
+    temp3=getnode(3);
+
+    temp1->next=temp2;
+    temp2->next=temp3;
+    return temp1;
+}
+
+void display(Node *s){//display a value of list  
+    while(s!=NULL){
+     printf("%d ",s->data+1);
+     s=s->next;
+    }
+}
